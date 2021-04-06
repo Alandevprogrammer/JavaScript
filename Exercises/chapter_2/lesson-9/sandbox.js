1ba00bbc8ca5a1681ccae477f9a69cbb15f33300
Alan <76871498+Alandevprogrammer@users.noreply.github.com>
2021-04-06 14:47:41 -0300

    modern-javascript / Chapter_2/ lesson-9

---
+++ Exercises/chapter_2/lesson-9/sandbox.js
@@ -0,0 +1,27 @@
+// strings
+console.log('hello, world');
+
+let email = 'mariocard@nintendo.co.us';
+console.log(email);
+
+// string concatenation
+let firstName = 'Henry';
+let lastName = 'Black';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+// getting individual characters
+console.log(fullName[3]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLocaleLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log('index of the @ sign:', index);
