efd7b31fe6a5ec9f49ecc4b62eb4d3e3ed85177e
Alan <76871498+Alandevprogrammer@users.noreply.github.com>
2021-04-06 13:37:01 -0300

    lesson-8

---
+++ Exercises/chapter_2/lesson-8/sandbox.js
@@ -0,0 +1,14 @@
+let age = 16;
+let year = 2021;
+
+console.log(age, year);
+
+age = 23;
+console.log(age);
+
+// const points = 834;
+// points = 345;
+// console.log(points);
+
+// var score = 234;
+// console.log(score);
