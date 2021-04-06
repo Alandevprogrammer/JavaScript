a9f4198d1659e973b88fa397ee5a6c1e18a98d2a
Alan <76871498+Alandevprogrammer@users.noreply.github.com>
2021-04-06 14:28:33 -0300

    Update sandbox.js

--- Exercises/chapter_2/lesson-8/sandbox.js
+++ Exercises/chapter_2/lesson-8/sandbox.js
@@ -1,14 +1,16 @@
 let age = 16;
 let year = 2021;
 
+// log things to console
 console.log(age, year);
 
 age = 23;
 console.log(age);
 
 const points = 100;
-// points = 57;
-// console.log(points);
+/* points = 57;
+ console.log(points); */
+// for comments on multiple lines, use / * at the beginning and * /
 
 var score = 90;
 console.log(score);
