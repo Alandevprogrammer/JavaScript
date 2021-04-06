ac84da1ef8bebbcc7e857481964dd9b4f8ac0f8d
Alan <76871498+Alandevprogrammer@users.noreply.github.com>
2021-04-06 16:04:36 -0300

    modern-javascript / Chapter_2/ lesson-10

---
+++ Exercises/chapter_2/lesson-10/sandbox.js
@@ -0,0 +1,15 @@
+// common string methods
+
+let email = 'henryevans@gmail.co.uk'
+
+// let result = email.lastIndexOf('i');
+
+// let result = email.slice(0, 14);
+
+// let result = email.substr(4, 10);
+
+// let result = email.replace('m', 'p');
+
+let result = email.replace('e', '3');
+
+console.log(result);
