92ccdae0d44258b48a7a417de68047a4a42c9b61
Alan <76871498+Alandevprogrammer@users.noreply.github.com>
2021-04-13 14:43:37 -0300

    Exercises\chapter_2\lesson-11

---
+++ exercises/chapter_2/lesson-11/sandbox.js
@@ -0,0 +1,46 @@
+// numbers
+
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+//console.log(10 / 2);
+//let result = radius % 3;
+//let result = pi * radius ** 2;
+//area of ​​a circle
+
+// order of operation - B I D M A S
+
+//let result = 5 * (10-3)**2;
+
+//console.log(result);
+
+let likes = 56;
+
+//likes = likes + 4;
+//likes++;
+//likes--;
+
+//likes += 10;
+//likes -= 5;
+//likes *= 56;
+//likes /= 2;
+
+//console.log(likes);
+
+// NaN not a number
+
+//console.log(10 / 'hello');
+//console.log(10 * 'helllo');
+
+let result = 'the blog has' + ' ' + likes + ' ' + 'likes';
+
+console.log(result);
+
+
+
+
+
