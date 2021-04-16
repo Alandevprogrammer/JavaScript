8044c5b1ab9487171e082229417749194339f3ef
Alan <alanpessoa02@gmail.com>
2021-04-16 18:52:54 -0300

    Exercises\chapter_2\lesson-16

---
+++ exercises/chapter_2/lesson-16/sandbox.js
@@ -0,0 +1,16 @@
+let age = 25;
+
+// loose comparison (diferrent types can still be equal)
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+// strict comparison (diferrent types cannot be equal)
+
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
+
