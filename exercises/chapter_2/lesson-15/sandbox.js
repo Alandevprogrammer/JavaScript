3aee052020e62670bb6bd97e10c6929c09434d70
Alan <alanpessoa02@gmail.com>
2021-04-16 16:27:37 -0300

    Exercises\chapter_2\lesson-15
    Booleans

---
+++ exercises/chapter_2/lesson-15/sandbox.js
@@ -0,0 +1,31 @@
+// booleans e comparisons
+console.log(true, false, 'true', 'false');
+
+// methods can return booleans
+let email = 'giuseppe@thecompany.co.it';
+let names = ['mario','luigi', 'maria'];
+
+// let result = email.includes('!');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 25);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'camille';
+
+console.log(name == 'camille');
+console.log(name == 'Camille');
+console.log(name > 'oliver queen');
+console.log(name > 'Camille');
+console.log(name > 'Samille');
+
