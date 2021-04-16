dd49d84f58b1de6688ef368b0e284437bd57e360
Alan <alanpessoa02@gmail.com>
2021-04-16 13:53:17 -0300

    Create sandbox.js

---
+++ exercises/chapter_2/lesson-13/sandbox.js
@@ -0,0 +1,15 @@
+let ninjas = ['diego', 'emilly', 'chun-li'];
+
+// ninjas[1] = 'katy'
+// console.log(ninjas[1])
+
+// let ages = [24, 27, 35, 30];
+// console.log(ages[2]);
+
+// let random = ['harry', 'diamond', 30, 21];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
